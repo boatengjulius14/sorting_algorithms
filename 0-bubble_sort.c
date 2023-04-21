65810c30a65d92ffebbb5cdeb66f45e20783762f
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-04-21 12:26:19 +0100

    solution for task 0 - 0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,12 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, next_val, curr_val, check_sorted;
+	size_t i, j, check, next_val, curr_val;
 
 	if ((array == NULL) || (size == 1))
 		return;
 
-	check_sorted = bubble_best_case(array, size);
-	if (check_sorted == 0)
-		return;
+	check = 0;
 
 	for (i = 0; i < (size - 1); i++)
 	{
@@ -30,45 +28,14 @@ void bubble_sort(int *array, size_t size)
 				array[j] = next_val;
 				array[j + 1] = curr_val;
 
+				check = 1;
+
 				/* print array after swap */
 				print_array(array, size);
 			}
 		}
+
+		if (check == 0)
+			break;
 	}
-}
-
-/**
- * bubble_best_case - check for best case
- *
- * @array: array of integers to be sorted
- * @size: size of array
- *
- * Return: 0 if best case; 1 otherwise
- */
-
-int bubble_best_case(int *array, size_t size)
-{
-	int check;
-	size_t j, curr_val, next_val;
-
-	check = 0;
-
-	for (j = 0; j < (size - 1); j++)
-	{
-		curr_val = array[j];
-		next_val = array[j + 1];
-
-		if (curr_val > next_val)
-		{
-			array[j] = next_val;
-			array[j + 1] = curr_val;
-
-			check = 1;
-		}
-	}
-
-	if (check == 0)
-		return (0);
-
-	return (1);
 }

86efbb9cdec182476a35895a9cb8b235e8d1087b
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-04-22 14:36:24 +0100

    corrections for task 0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
-* selection_sort - sorts an array of integers in ascending order
-* using the Selection sort algorithm
-* @array: pointer to array;
-* @size: size of array;
-*/
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: pointer to array;
+ * @size: size of array;
+ */
 
 void selection_sort(int *array, size_t size)
 {
@@ -18,6 +18,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		low_pos = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[low_pos] > array[j])
@@ -26,12 +27,15 @@ void selection_sort(int *array, size_t size)
 				flag = 1;
 			}
 		}
-		tmp = array[i];
-		array[i] = array[low_pos];
-		array[low_pos] = tmp;
 
 		if (flag == 1)
+		{
+			tmp = array[i];
+			array[i] = array[low_pos];
+			array[low_pos] = tmp;
+			flag = 0;
+
 			print_array(array, size);
-		flag = 0;
+		}
 	}
 }
